d9bae5ce0f23d2df16c87c1afa082e73c5f65256
modtherhub <moibhub@gmail.com>
2026-02-17 01:40:35 +0400

    chang name of lesson 1 & 2, and work with nuber converting

---
+++ Lesson10/main.js
@@ -0,0 +1,15 @@
+// in the past lesson we discover the way to convert string to number
+// this we will daive into another way 
+
+console.log(+'4');
+console.log(Number('4'));
+console.log(+'4');
+console.log(parseInt('4 Modther'));
+console.log(parseFloat('4.5'));
+
+
+// if your number came with string like '4 Modther' in this situation you can use parseInt
+// parseInt will work when your number in the first else will return NaN
+// also when work with float '2.4' it will return 2
+// Camle case
+//we also use parseFloat
---
+++ Lesson9/main.js
@@ -0,0 +1,18 @@
+console.log("5" + "5") // this is concatination wil return string
+console.log("5" / "5") // this is math procces and will retuen number
+console.log("5" * "5")
+console.log("5" - "5")
+console.log("5" ** "5") // al this procces is mathimatics 
+// even when you use this 
+console.log("5" / "ahmed") // this will return NaN => is also is nunber
+
+// if you want sum sring number with without concatination write +"5"
+console.log(+"5" + +"5") // = 10  this will return number 
+console.log("5") // type of this is string
+console.log(+"5") // type of this is number 
+console.log(-"5") // this also will return number but -
+
+// null قيمة فارغة
+console.log(+null) // will return 0
+// undefined قيمه غير موجودة
+console.log(+undefined) // will return NaN 

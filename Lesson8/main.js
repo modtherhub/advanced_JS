ac8671273b88e06947dfb5de0373cc16192e44d1
modtherhub <moibhub@gmail.com>
2026-02-17 00:56:40 +0400

    concatenation in JS

---
+++ Lesson8/main.js
@@ -0,0 +1,11 @@
+let name = 'Modther'
+let age = 15
+console.log(name + age)
+console.log('15' + age ) //concatenation
+// + in js used in numbers => sum, and in string withe number => concatenation
+let x = name 
+let space = " "
+console.log(name + space + age)
+console.log(name + " " + age)
+console.log("My name is " + name + ", My age is " + age) // this is the first way to concatenation
+console.log(`My name is ${name}, My age is ${age}`) // this is the second way to concatenation

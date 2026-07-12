ccdfeb0592763d9ca6d529d57f042b64c2bfdda1
modtherhub <moibhub@gmail.com>
2026-07-13 01:27:13 +0400

    nested function

---
+++ closure/main.js
@@ -0,0 +1,14 @@
+// function sayHiBye(firstName, lastName) {
+
+//   // helper nested function to use below
+//   function getFullName() {
+//     return firstName + " " + lastName;
+//   }
+
+//   console.log( "Hello, " + getFullName() );
+//   console.log( "Bye, " + getFullName() );
+
+// }
+
+// sayHiBye("moodther", "abdalhag")
+

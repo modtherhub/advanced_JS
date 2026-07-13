3392e62c7642f21b983c5444592465c1b477ad02
modtherhub <moibhub@gmail.com>
2026-07-14 03:44:52 +0400

    FUNCTION & return expression

---
+++ function.js
@@ -0,0 +1,13 @@
+function multi(a, b) {
+    return a*b
+}
+
+
+console.log(multi(4,4))
+
+// The function code may contain one or more expressions. One or more of those can contain the return keyword, followed by a value the function returns when executed. 
+// When a return expression is reached the function stops, and the value after return is returned by the function. 
+// In case there is no return expression in the function code, the function returns undefined.
+// Dorman, Michael. Introduction to Web Mapping, CRC Press LLC, 2020. ProQuest Ebook Central, http://ebookcentral.proquest.com/lib/univ-people-ebooks/detail.action?docID=6029016.
+// Created from univ-people-ebooks on 2026-07-13 23:43:27.
+// TRY IT YOURSELF
